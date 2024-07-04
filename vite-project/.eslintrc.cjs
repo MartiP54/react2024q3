ca9d401feca253ff3d0a31ac3277cfc8ead47396
MartiP54 <116718918+MartiP54@users.noreply.github.com>
2024-07-04 18:43:23 +0700

    init: start class-components

---
+++ vite-project/.eslintrc.cjs
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json', './tsconfig.app.json', './tsconfig.node.json'],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-compiler',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react-compiler/react-compiler': 'error',
+  },
+};
