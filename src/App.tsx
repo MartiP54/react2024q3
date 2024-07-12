f8e134763781df898c7abf3a25555c786a03bc2a
MartiP54 <116718918+MartiP54@users.noreply.github.com>
2024-07-13 00:50:46 +0700

    feat: add routing to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,18 @@
 import './App.css';
-import { useState } from 'react';
-import Header from './components/header';
-import Content from './components/content';
-import ErrorBoundary from './components/errorBoundary';
-import useSearchQuery from './hooks/useSearchQuery';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 
 export default function App () {
-  const [query, setQuery] = useSearchQuery('lastSearchMarti');
-  const [searchKey, setSearchKey] = useState(0);
-
-  const handleSearch = (newQuery: string) => {
-    setQuery(newQuery);
-    setSearchKey(prevKey => prevKey + 1);
-  };
+  
 
     return (
       <div className="app_wrapper">
-        <ErrorBoundary fallback={<div>Sorry for the inconvenience. An error occurred, try loading the page again.</div>}>
-          {(setError) => (
-            <>
-              <Header onSearch={handleSearch} initialQuery={query} onError={setError} />
-              <Content searchKey={searchKey} />
-            </>
-          )}
-        </ErrorBoundary>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="*" element={<NotFound />} />
+              </Routes>
         </div>
     );
 
